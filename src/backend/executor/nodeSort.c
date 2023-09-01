8fa3ff0341ff5e1d7c08f21aad1b2834f1ed9e43
Shyandeep Das <93070088+ShyandeepDas@users.noreply.github.com>
2023-09-01 17:51:26 -0500

    Update nodeSort.c

--- src/backend/executor/nodeSort.c
+++ src/backend/executor/nodeSort.c
@@ -90,61 +90,61 @@ ExecSort(PlanState *pstate)
 
 		if(plannode->numCols<50){
 			Size_lt_fifty++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_lt_fifty = %d\n",Size_lt_fifty);
 			fclose(outfile);
 		}
 		else if(plannode->numCols<50 && plannode->numCols<100){
 			Size_gt_fifty_lt_hundred++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_fifty_lt_hundred = %d\n",Size_gt_fifty_lt_hundred);
 			fclose(outfile);
 		}
 		else if(plannode->numCols<100 && plannode->numCols<150){
 			Size_gt_hundred_lt_onefifty++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_hundred_lt_onefifty = %d\n",Size_gt_hundred_lt_onefifty);
 			fclose(outfile);
 		}
 		else if(plannode->numCols<150 && plannode->numCols<200){
 			Size_gt_onefifty_lt_twohundred++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_onefifty_lt_twohundred = %d\n",Size_gt_onefifty_lt_twohundred);
 			fclose(outfile);
 		}
 		else if(plannode->numCols<200 && plannode->numCols<250){
 			Size_gt_twohundred_lt_twofifty++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_twohundred_lt_twofifty = %d\n",Size_gt_twohundred_lt_twofifty);
 			fclose(outfile);
 		}
 		else if(plannode->numCols<250 && plannode->numCols<300){
 			Size_gt_twofifty_lt_threehundred++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_twofifty_lt_threehundred = %d\n",Size_gt_twofifty_lt_threehundred);
 			fclose(outfile);
 		}
 		else if(plannode->numCols<300 && plannode->numCols<350){
 			Size_gt_threehundred_lt_threefifty++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_threehundred_lt_threefifty = %d\n",Size_gt_threehundred_lt_threefifty);
 			fclose(outfile);
 		}
 		else if(plannode->numCols>350 && plannode->numCols<400){
 			Size_gt_threefifty_lt_fourhundred++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_threefifty_lt_fourhundred = %d\n",Size_gt_threefifty_lt_fourhundred);
 			fclose(outfile);
 		}
 		else if(plannode->numCols>400 && plannode->numCols<1000){
 			Size_gt_fourhundred_lt_thousand++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_fourhundred_lt_thousand = %d\n",Size_gt_fourhundred_lt_thousand);
 			fclose(outfile);
 		}
 		else{
 			Size_gt_thousand++;
-			FILE *outfile = fopen("tabsize.txt", "a");
+			FILE *outfile = fopen("/home/ubuntu/tabsize.txt", "a");
 			fprintf(outfile, "Size_gt_thousand = %d\n",Size_gt_thousand);
 			fclose(outfile);
 		}		
